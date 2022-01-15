bb2bfdf02efefddcf24a85758fe44ce00f296542
ScofieldaCreep <tju_scofield@outlook.com>
2022-01-15 19:54:32 +0800

    hw08 finished

--- homework/hw08/hw08.scm
+++ homework/hw08/hw08.scm
@@ -2,7 +2,7 @@
   (cdr (cdr s)))
 
 (define (cadr s)
-  (cdr (car s))
+  (car (cdr s))
 )
 
 (define (caddr s)
@@ -10,21 +10,39 @@
 )
 
 (define (sign x)
-  'YOUR-CODE-HERE
+  (cond
+    ((< x 0) -1)
+    ((= x 0) 0)
+    ((> x 0) 1)
+    )
 )
 
 (define (square x) (* x x))
 
 (define (pow b n)
-  'YOUR-CODE-HERE
+    (cond
+        ((= n 1) b)
+        ((even? n) (square (pow b (/ n 2))))
+        ((odd? n) (* b (pow b (- n 1))))
+    )
 )
 
+
 (define (ordered? s)
-  'YOUR-CODE-HERE
+    (cond
+        ( (null? s) true)
+        ( (null? (cdr s)) true)
+        ( (> (car s) (cadr s)) false)
+        (else (ordered? (cdr s))))
 )
 
 (define (nodots s)
-  'YOUR-CODE-HERE
+    (cond
+        ((null? s) nil)
+        ((not (pair? s)) (cons s nil))
+        ((pair? (car s)) (cons (nodots (car s)) (nodots (cdr s))))
+        (else (cons (car s) (nodots (cdr s))))
+        )
 )
 
 ; Sets as sorted lists
@@ -33,8 +51,8 @@
 
 (define (contains? s v)
     (cond ((empty? s) #f)
-          'YOUR-CODE-HERE
-          (else nil) ; replace this line
+          ((= (car s) v) #t)
+          (else (contains? (cdr s) v))
           ))
 
 ; Equivalent Python code, for your reference:
@@ -53,15 +71,17 @@
 ;         return contains(s.rest, v)
 
 (define (add s v)
-    (cond ((empty? s) (list v))
-          'YOUR-CODE-HERE
-          (else nil) ; replace this line
+    (cond ( (empty? s) (list v))
+          ( (< v (car s)) (cons v s))
+          ( (= v (car s)) s)
+          ( (> v (car s)) (cons (car s) (add (cdr s) v)))
           ))
 
 (define (intersect s t)
     (cond ((or (empty? s) (empty? t)) nil)
-          'YOUR-CODE-HERE
-          (else nil) ; replace this line
+          ( (< (car s) (car t)) (intersect (cdr s) t))
+          ( (= (car s) (car t)) (cons (car s) (intersect (cdr s) (cdr t))))
+          ( (> (car s) (car t)) (intersect s (cdr t)))
           ))
 
 ; Equivalent Python code, for your reference:
@@ -81,6 +101,7 @@
 (define (union s t)
     (cond ((empty? s) t)
           ((empty? t) s)
-          'YOUR-CODE-HERE
-          (else nil) ; replace this line
+          ( (< (car s) (car t)) (cons (car s) (union (cdr s) t)))
+          ( (= (car s) (car t)) (cons (car s) (union (cdr s) (cdr t))))
+          ( (> (car s) (car t)) (cons (car t) (union s (cdr t))))
           ))
